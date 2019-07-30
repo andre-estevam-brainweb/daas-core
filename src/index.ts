86ff3b1d29a447ca78f283a20107cf40a719fd94
andre-estevam-brainweb <andre.estevam@brainweb.com.br>
2019-07-30 11:41:52 -0300

    just adding some console logs temporarily

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,11 @@ import { wait } from "./support/wait"
 let manager: LobbyManager
 
 async function init(comms: Communications) {
+
+	console.log('[ INITIATING DAAS CORE ] call: init()')
+
 	await comms.sendMessage(MessageType.BOOT_OK)
-	console.log(
-		"Sent BOOT_OK message to provider, now waiting for its response..."
-	)
+	console.log("Sent the BOOT_OK message to provider, now waiting for its response...")
 
 	const { info: { botId } } = await comms.waitForMessage(
 		MessageType.DOTA_BOT_INFO,
@@ -25,37 +26,42 @@ async function init(comms: Communications) {
 	)
 	console.log(`The provider sent the bot ID ${botId}. Querying bot...`)
 
-	const bot =
-		(await Bots.findById(botId)) ||
-		(() => {
-			throw new Error("The bot doesn't exist")
-		})()
+	const bot = (await Bots.findById(botId)) || (() => {
+		throw new Error("The bot doesn't exist")
+	})()
 
 	console.log(
-		`Attempting to log in Steam and connect to the Dota GC with bot ${
-			bot.username
-		}...`
+		`Attempting to log in Steam and connect to the Dota GC with bot ${bot.username}...`
 	)
-	const dota = await getDotaClient(bot)
-	console.log("Connection successful, waiting for provider instructions...")
 
+	const dota = await getDotaClient(bot)
+
+	console.log("Connection successful, waiting for provider instructions...")
+	console.log("DOTA CLIENT: ", dota)
+
+	console.log('Setting the bot status as IDLE')
 	await Bots.update(bot, { status: BotStatus.IDLE })
 
+	console.log('Sending the DOTA_OK message')
 	await comms.sendMessage(MessageType.DOTA_OK)
 
 	comms.adapterMessageStream
 		.filter(it => it.type === MessageType.KILL_YOURSELF)
 		.take(1)
 		.subscribe(() => {
-			console.log("The provider told me to kill myself. Let's do this.")
+			console.log("The provider told me to kill myself. Let's do this. Goodbye cruel world.")
 			gracefulShutdown(comms, bot).catch(console.error)
 		})
 
 	const response = await comms.waitForMessage(MessageType.LOBBY_INFO)
 
+
 	if (!response) {
+		console.log('No lobby received')
 		return
 	}
+
+	console.log('A LOBBY_INFO message was received, content:', response)
 
 	const { info: { lobbyId } } = response
 
@@ -70,7 +76,11 @@ async function init(comms: Communications) {
 	console.log(`Attempting to create lobby '${lobby.name}'...`)
 	manager = await LobbyManager.create(comms, dota, lobby)
 
+
+	console.log('Setting the bot status as IN_LOBBY')
 	await Bots.update(bot, { status: BotStatus.IN_LOBBY })
+
+	console.log('Sending the LOBBY_OK message')
 	await comms.sendMessage(MessageType.LOBBY_OK)
 
 	// Whenever a player status changes, save it to the database, and
@@ -106,13 +116,19 @@ async function init(comms: Communications) {
 		"index/resendInviteListener"
 	)
 
+	console.log('Waiting for the lobby to finish')
 	const finalStatus = await manager.waitUntilResultOrCancellation()
+
+	console.log('Lobby finished, final status is', finalStatus)
 	await Lobbies.update(lobby, { status: finalStatus })
+
+	console.log('Sending the GAME_FINISHED message')
 	await comms.sendMessage(MessageType.GAME_FINISHED)
 
 	console.log(`The final lobby status is ${LobbyStatus[finalStatus]}`)
 	await wait(5000)
 
+	console.log('Shutting down the machine, bye')
 	await gracefulShutdown(comms, bot, manager)
 }
 
@@ -142,6 +158,8 @@ async function gracefulShutdown(
 }
 
 async function main() {
+	console.log('[ INITIATING DAAS CORE ] call: main()')
+
 	const requestId = process.argv[2]
 
 	if (!requestId) {
